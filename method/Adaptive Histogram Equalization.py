4c20c64c3cbd6014ae6de375e3805189a0efe25f
howard-hihihi <howard930513@gmail.com>
2024-04-09 13:50:13 +0800

    new

--- method/Adaptive Histogram Equalization.py
+++ method/Adaptive Histogram Equalization.py
@@ -5,13 +5,16 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-img_dir = "tooth"
+img_dir = r"C:\Users\user\Desktop\dataset\periapical film\Original Images\101~200"
+blur_dir = r"C:\Users\user\Desktop\dataset\periapical film\Median_Blur"
 output_dir = "output_images"
 
+os.makedirs(blur_dir, exist_ok=True)
 os.makedirs(output_dir, exist_ok=True)
 
 for name in os.listdir(img_dir):
     img_path = os.path.join(img_dir, name)
+    blur_path = os.path.join(blur_dir, name)
     output_path = os.path.join(output_dir, name)
 
     # 讀取灰度照片
@@ -26,7 +29,8 @@ for name in os.listdir(img_dir):
     # 適應性直方圖均衡化
     clahe_img = clahe.apply(filtered_image)
 
-    cv2.imwrite(output_path, clahe_img)
+    cv2.imwrite(blur_path, filtered_image)
+    # cv2.imwrite(output_path, clahe_img)
     # print(f'save image "{output_path}"')
 
     # # 顯示原始與增強後的影像
